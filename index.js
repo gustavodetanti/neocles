147848111949e4973dbe450b16c6ee84639139de
Gustavo De Tanti <gustavodetanti@gmail.com>
2022-04-12 03:49:35 -0300

    test1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.onload=function(){
 
+documen.querySelector('.app').innerHTML='neocles';
 
+return;
     navigator.serviceWorker.register('swcacheall.js').then((resp)=>{
 
         console.log("register",resp);
